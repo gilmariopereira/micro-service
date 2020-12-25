f4c06d46ef994e9ad5293aa7fabf67884528adfb
gilmario <gilmario_division@hotmail.com>
2020-12-25 14:04:33 -0300

    Eureka Client Random port

--- hr-payroll/src/main/java/com/gilmariopereira/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/gilmariopereira/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,10 @@ package com.gilmariopereira.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-workeer")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
