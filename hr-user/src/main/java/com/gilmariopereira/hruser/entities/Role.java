4c7841c3ddc70d0080fef3616988d1e4ddc9db98
gilmario <gilmario_division@hotmail.com>
2020-12-30 23:44:32 -0300

    User, Role, Many to Many

---
+++ hr-user/src/main/java/com/gilmariopereira/hruser/entities/Role.java
@@ -0,0 +1,54 @@
+package com.gilmariopereira.hruser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Role;
+	
+	public Role() {
+			
+	}
+	public Role(Long id, String role) {
+		super();
+		this.id = id;
+		Role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return Role;
+	}
+
+	public void setRole(String role) {
+		Role = role;
+	}
+	
+	
+	
+
+}
