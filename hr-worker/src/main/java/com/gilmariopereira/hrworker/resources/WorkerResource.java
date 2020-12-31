e4d38a77a393642f985c9fc6c8e2a4ed5b8efee7
gilmario <gilmario_division@hotmail.com>
2020-12-30 23:25:26 -0300

    Config Server

--- hr-worker/src/main/java/com/gilmariopereira/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/gilmariopereira/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import com.gilmariopereira.hrworker.entities.Worker;
 import com.gilmariopereira.hrworker.repositories.WorkerRepository;
 
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
